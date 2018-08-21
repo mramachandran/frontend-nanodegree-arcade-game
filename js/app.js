c3c753037122b17d737430d8dce7e47a1b90d903
Manoj Ramachandran <m.ramachandran@craneware.com>
2018-08-20 21:46:21 -0400

    good update on canvas.. able to get the objects to appear and move

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 1 * 101
+    this.y = 2 * 83
 };
 
 // Update the enemy's position, required method for game
@@ -14,22 +16,72 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //dt = dt*2
+    this.x += this.x * dt
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    //ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+        this.xBoxID = 1;
+        this.yBoxID = 5;
+        this.xBoxIDMultiplier = 101;
+        this.yBoxIDMultiplier = 83;
+
+        this.x = this.xBoxID * this.xBoxIDMultiplier;
+        this.y = this.yBoxID * this.yBoxIDMultiplier;
+    }
+    handleInput(keys) {
+            console.log(keys)
+
+            switch(keys) {
+                case 'up':
+                    this.yBoxID = this.yBoxID-1;
+                    break;
+                case 'down':
+                    this.yBoxID = this.yBoxID+1;
+                    break;
+                case 'left':
+                    this.xBoxID = this.xBoxID-1;
+                    break;    
+                case 'right':
+                    this.xBoxID = this.xBoxID+1;
+                    break;                                     
+                default:
+                    break; 
+            }
+      };
+
+      update() {
+        this.x = this.xBoxID * this.xBoxIDMultiplier;
+        this.y = this.yBoxID * this.yBoxIDMultiplier;
+      }
+
+      render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  
+      }
+
+}
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const enemy = new Enemy();
+allEnemies = [enemy];
 
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -149,6 +149,7 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+        //console.log(enemy);
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
